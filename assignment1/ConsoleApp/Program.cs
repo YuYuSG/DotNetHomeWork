7522a24cde5b88473faa05e083df1c83c4c1fc2e
YuYuSG <3118983205@qq.com>
2023-03-06 23:46:00 +0800

    week3
    assignment1
    assignment2

---
+++ assignment1/ConsoleApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "";
+            int l = 0, r = 0;
+            char oper;
+         
+            Console.Write("Please input an integer number: ");
+            s = Console.ReadLine();
+            l = Int32.Parse(s);
+            Console.Write("Please input another integer number: ");
+            s = Console.ReadLine();
+            r = Int32.Parse(s);
+            Console.Write("Please input an operator(+,-,*,/,%): ");
+            s = Console.ReadLine();
+            oper = Char.Parse(s);
+
+            switch (oper)
+            {
+                case '+': Console.Write($"result:{ l+r}");break;
+                case '-': Console.Write($"result:{ l - r}"); break;
+                case '*': Console.Write($"result:{ l * r}"); break;
+                case '/': Console.Write($"result:{ l / r}"); break;
+                case '%': Console.Write($"result:{ l % r}"); break;
+                default: Console.Write("Please input the correct operator.");break;
+
+            }
+
+        }
+    }
+}
