e47eee5af9255c07dbbfb3709bd97cd116120e50
YuYuSG <3118983205@qq.com>
2023-03-12 16:34:09 +0800

    assignment3
    ...

---
+++ assignment3/ShapeClass/ShapeClass.cs
@@ -0,0 +1,169 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Interfaces.Streaming;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ShapeClass
+{
+    //形状类
+    public abstract class Shape
+    {
+        protected bool validity = false;
+        protected double area = 0;
+
+        public bool GetValidity() { return validity; }
+        public double GetArea() { return area; }
+        public abstract void Judge();
+        public abstract void Calculate();
+
+    }
+    
+    //三角形类
+    public class Triangle : Shape
+    {
+        protected double a = 0;
+        protected double b = 0;
+        protected double c = 0;
+
+        public Triangle(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public override void Judge()
+        {
+            if (a <= 0 || b <= 0 || c <= 0 || (a + b) <= c || (b + c) <= a || (c + a) <= b)
+                return;
+            validity = true;
+        }
+        public override void Calculate()
+        {
+            if (!validity)
+                return;
+            double p = (a + b + c) / 2;
+            area = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+    }
+
+    //长方形类
+    public class Rectangle : Shape
+    {
+        protected double width = 0;
+        protected double length = 0;
+
+        public Rectangle() { }
+        public Rectangle(double w, double l)
+        {
+            width = w;
+            length = l;
+        }
+
+        public override void Judge()
+        {
+            if (width <= 0 || length <= 0)
+                return;
+            validity = true;
+        }
+        public override void Calculate()
+        {
+            if (!validity)
+                return;
+            area = width * length;
+        }
+
+
+    }
+
+    //正方形类
+    public class Square : Rectangle
+    {
+        public Square() { }
+        public Square(double edge)
+        {
+            width = edge;
+            length = edge;
+        }
+        
+        public override void Judge()
+        {
+            if (width <= 0 || length <= 0 || width != length)
+                return;
+            validity = true;
+        }
+
+    }
+
+    //椭圆类
+    public class Ellipse : Shape
+    {
+        protected double a = 0;
+        protected double b = 0;
+
+        public Ellipse() { }
+        public Ellipse(double a, double b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public override void Judge()
+        {
+            if (a <= 0 || b <= 0)
+                return;
+            validity = true;
+        }
+        public override void Calculate()
+        {
+            if (!validity)
+                return;
+            area = 3.14 * a * b;
+        }
+    }
+
+    //圆类
+    public class Circle : Ellipse
+    {
+        public Circle() { }
+        public Circle(double r)
+        {
+            a = r;
+            b = r;
+        }
+
+        public override void Judge()
+        {
+            if (a <= 0 || b <= 0 || a != b)
+                return;
+            validity = true;
+        }
+
+    }
+
+    //形状判断类
+    public class ShapeJudegement
+    {
+        public ShapeJudegement() { }
+        public bool JudgeShape(Shape shape)
+        {
+            shape.Judge();
+            return shape.GetValidity();
+        }
+    }
+
+    //形状面积计算类
+    public class ShapeCalculation
+    {
+        public double CalculateShape(Shape shape)
+        {
+            shape.Judge();
+            shape.Calculate();
+            return shape.GetArea();
+        }
+    }
+}
---
+++ assignment3/ShapeObject/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShapeClass;
+
+namespace ShapeObject
+{
+    class ShapeObject
+    {
+        static double SumOfShapes(Shape[] shapes)
+        {
+            double sumOfShapes = 0;
+            ShapeCalculation C = new ShapeCalculation();
+            for(int i = 0; i < shapes.Length; i++)
+            {
+                sumOfShapes += C.CalculateShape(shapes[i]);
+            }
+            return sumOfShapes;
+        }
+        
+        
+        static void Main(string[] args)
+        {
+            Shape[] shapes = new Shape[10];
+            Random R = new Random();
+            double sumOfShapes = 0;
+            for(int i = 0; i < 10; i++)
+            {
+                int type = R.Next(0, 5);
+                switch (type)
+                {
+                   case 0:
+                        {
+                            double a = R.Next(25)+R.NextDouble();
+                            double b = R.Next(25) + R.NextDouble();
+                            double c = R.Next(25) + R.NextDouble();
+                            shapes[i] = new Triangle(a, b, c);
+                            Console.WriteLine($"第{i+1}个形状为三角形，边长分别为：{a}，{b}，{c}");
+                            break;
+                        }
+                    case 1:
+                        {
+                            double w = R.Next(25) + R.NextDouble();
+                            double l = R.Next(25) + R.NextDouble();
+                            shapes[i] = new Rectangle(w,l);
+                            Console.WriteLine($"第{i + 1}个形状为长方形，宽为：{w}，长为{l}");
+                            break;
+                        }
+                    case 2:
+                        {
+                            double e = R.Next(25) + R.NextDouble();
+                            shapes[i] = new Square(e);
+                            Console.WriteLine($"第{i + 1}个形状为正方形，边长为：{e}");
+                            break;
+                        }
+                    case 3:
+                        {
+                            double a = R.Next(25) + R.NextDouble();
+                            double b = R.Next(25) + R.NextDouble();
+                            shapes[i] = new Ellipse(a, b);
+                            Console.WriteLine($"第{i + 1}个形状为椭圆形，半长轴为：{a}，半短轴为{b}");
+                            break;
+                        }
+                    case 4:
+                        {
+                            double r = R.Next(25) + R.NextDouble();
+                            shapes[i] = new Circle(r);
+                            Console.WriteLine($"第{i + 1}个形状为圆形，半径为：{r}");
+                            break;
+                        }
+                    defualt: break;
+
+                }
+            }
+            sumOfShapes = SumOfShapes(shapes);
+            Console.WriteLine($"10个形状对象的面积之和：{sumOfShapes}");
+
+
+        }
+    }
+}
+
